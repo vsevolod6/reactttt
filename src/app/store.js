44a2e685a7c69e321608a1559d6be1e8de3b4968
vsevolod6 <ZakurnaevVD23@st.ithub.ru>
2026-01-18 01:17:46 +0300

    Initialize Redux store with students reducer
    Set up Redux store with students reducer and DevTools.

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import studentsReducer from '../parts/students/studentsSlice'
+
+export default configureStore({
+  reducer: {
+    students: studentsReducer
+  },
+  devTools: process.env.NODE_ENV !== 'production', // включаем DevTools
+})
