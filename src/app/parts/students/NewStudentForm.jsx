a825747c88ea40ff8c7419cc05ec46cdcf35495a
vsevolod6 <ZakurnaevVD23@st.ithub.ru>
2026-01-18 03:23:07 +0300

    Refactor NewStudentForm component

--- src/app/parts/students/NewStudentForm.jsx
+++ src/app/parts/students/NewStudentForm.jsx
@@ -4,26 +4,20 @@ import { nanoid } from '@reduxjs/toolkit'
 import { studentAdded } from './studentsSlice'
 
 export const NewStudentForm = () => {
-  // Локальные стейты для полей формы
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [age, setAge] = useState(0)
   const [specialty, setSpecialty] = useState('')
 
-  // Доступ к dispatch
   const dispatch = useDispatch()
 
-  // Обработчики изменений полей
   const onFirstNameChanged = (e) => setFirstName(e.target.value)
   const onLastNameChanged = (e) => setLastName(e.target.value)
   const onAgeChanged = (e) => setAge(e.target.value)
   const onSpecialtyChanged = (e) => setSpecialty(e.target.value)
 
-  // Обработчик клика по кнопке "Save"
   const onSaveStudentClick = () => {
-    // Проверка, что все поля заполнены
     if (firstName && lastName && age && specialty) {
-      // Отправка action в Redux store
       dispatch(
         studentAdded({
           id: nanoid(),
@@ -33,8 +27,6 @@ export const NewStudentForm = () => {
           specialty,
         })
       )
-
-      // Сброс локальных стейтов
       setFirstName('')
       setLastName('')
       setAge(0)
@@ -50,7 +42,6 @@ export const NewStudentForm = () => {
           <label htmlFor="studentFirstName">First Name:</label>
           <input
             id="studentFirstName"
-            name="studentFirstName"
             value={firstName}
             onChange={onFirstNameChanged}
           />
@@ -59,7 +50,6 @@ export const NewStudentForm = () => {
           <label htmlFor="studentLastName">Last Name:</label>
           <input
             id="studentLastName"
-            name="studentLastName"
             value={lastName}
             onChange={onLastNameChanged}
           />
@@ -68,7 +58,6 @@ export const NewStudentForm = () => {
           <label htmlFor="studentAge">Age:</label>
           <input
             id="studentAge"
-            name="studentAge"
             type="number"
             value={age}
             onChange={onAgeChanged}
@@ -78,15 +67,14 @@ export const NewStudentForm = () => {
           <label htmlFor="studentSpecialty">Specialty:</label>
           <input
             id="studentSpecialty"
-            name="studentSpecialty"
             value={specialty}
             onChange={onSpecialtyChanged}
           />
         </p>
-        <button type="button" onClick={onSaveStudentClick}>
-          Save
-        </button>
+        <button type="button" onClick={onSaveStudentClick}>Save</button>
       </form>
     </div>
   )
 }
+
+
