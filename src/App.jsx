579abc281d75d2f4cd4429c861f0c5eb755d2b21
vsevolod6 <ZakurnaevVD23@st.ithub.ru>
2026-01-25 21:09:10 +0300

    Add StudentsList route to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './app/root'
+import { StudentsList } from './parts/students/StudentsList'
 
-const router = createBrowserRouter([{ path: '/', element: <Root /> }])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/students',
+        element: <StudentsList />,
+      },
+    ],
+  },
+])
 
 function App() {
   return <RouterProvider router={router} />
